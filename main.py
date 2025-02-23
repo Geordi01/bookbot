c3fe4b7d96459fe75246b0cfd8811ef949c09541
Geordi01 <gjad.bonnie@gmail.com>
2025-02-23 13:06:46 +0100

    finished updated bookbot course

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from stats import count_words, count_char, sort_char, get_book_text
 
 def main():
     print(sys.argv)
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = count_words(text)
     char_dict = count_char(text)
@@ -13,7 +13,7 @@ def main():
     print(f"{num_words} words found in the document")
     print("")
     for char in char_sort:
-        print(f"The '{char["char"]}' character was found {char["count"]} times")
+        print(f"{char["char"]}: {char["count"]}")
     print("--- End report ---")
 
 main()
